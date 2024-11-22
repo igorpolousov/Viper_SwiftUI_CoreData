4085b734627f7841d1478aea7aec471768886361
Igor Polousov <igorpolousov@gmail.com>
2024-11-22 14:46:59 +0300

    Initial Commit

---
+++ Viper_SwiftUI_CoreData/Viper_SwiftUI_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viper_SwiftUI_CoreDataApp.swift
+//  Viper_SwiftUI_CoreData
+//
+//  Created by Igor Polousov on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Viper_SwiftUI_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
