df406d88e78a59b3b6a8404a7c41da647e19bf03
Igor Polousov <igorpolousov@gmail.com>
2024-11-27 15:23:28 +0300

    - remove unneccessery lines in mainscreenview

--- Viper_SwiftUI_CoreData/Views/MainScreenViews/MainScreenView.swift
+++ Viper_SwiftUI_CoreData/Views/MainScreenViews/MainScreenView.swift
@@ -12,42 +12,36 @@ struct MainScreenView: View {
     @State private var searchText: String = ""
     @State var taskDescription: String = ""
     
-    @State var tasksData = TasksMockData()
-    
-
     var body: some View {
-        ZStack {
-            NavigationView {
-                VStack(alignment: .leading){
-                    List {
-                        ForEach(tasksData.tasksMockData) { task in
-                            ZStack {
-                                NavigationLink(destination: DetailScreenView()) {}
-                                .buttonStyle(.plain)
-                                .opacity(0.0)
-                                .frame(height: 0)
-                                
-                                TaskListRowView(taskTitle: task.taskName, taskDescription: task.taskDescription)
-                                 
-                            }
-                            .listRowBackground(Color.red)
-                            .listRowInsets(.init(top: 0,
-                                                   leading: 0,
-                                                   bottom: 0,
-                                                   trailing: 0))
-                        }
+        NavigationView {
+            List {
+                ForEach(TasksMockData.tasksMockData) { task in
+                    ZStack {
+                        NavigationLink(destination: DetailScreenView( taskDescription: $taskDescription)) {}
+                            .buttonStyle(.plain)
+                            .opacity(0.0)
+                            .frame(height: 0)
+                        
+                        TaskListRowView(taskTitle: task.taskName, taskDescription: taskDescription)
+                        
                     }
-                    .buttonStyle(BorderlessButtonStyle())
-                    .listStyle(.insetGrouped)
-                    .scrollContentBackground(.hidden)
-                    .background(Color.black)
+                    .listRowInsets(.init(top: 0,
+                                         leading: 0,
+                                         bottom: 0,
+                                         trailing: 0))
+                    
                 }
-                .navigationTitle("Tasks")
-                .toolbarBackground(.visible)
-                .toolbarColorScheme(.dark, for: .navigationBar)
-                .searchable(text: $searchText)
             }
+            .buttonStyle(BorderlessButtonStyle())
+            .listStyle(.insetGrouped)
+            .scrollContentBackground(.hidden)
+            .background(Color.black)
+            .navigationTitle("Tasks")
+            .toolbarBackground(.visible)
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .searchable(text: $searchText)
         }
+        
     }
 }
 
--- Viper_SwiftUI_CoreData/Views/MainScreenViews/SwiftUIView.swift
+++ Viper_SwiftUI_CoreData/Views/MainScreenViews/SwiftUIView.swift
@@ -12,37 +12,36 @@ struct SwiftUIView: View {
     @State private var searchText: String = ""
     @State var taskDescription: String = ""
     
-    @State var tasksData = TasksMockData()
-    
     var body: some View {
-        ZStack{
-            NavigationView {
-                VStack {
-                    List {
-                        ForEach(TasksMockData.tasksMockData) { task in
-                            ZStack {
-                                NavigationLink(destination: DetailScreenView( taskDescription: $taskDescription)) {}
-                                    .buttonStyle(.plain)
-                                    .opacity(0.0)
-                                    .frame(height: 0)
-                                
-                                TaskListRowView(taskTitle: task.taskName, taskDescription: taskDescription)
-
-                            }
-                            .listRowInsets(.init(top: 0,
-                                                 leading: 0,
-                                                 bottom: 0,
-                                                 trailing: 0))
-                            
-                        }
+        NavigationView {
+            List {
+                ForEach(TasksMockData.tasksMockData) { task in
+                    ZStack {
+                        NavigationLink(destination: DetailScreenView( taskDescription: $taskDescription)) {}
+                            .buttonStyle(.plain)
+                            .opacity(0.0)
+                            .frame(height: 0)
+                        
+                        TaskListRowView(taskTitle: task.taskName, taskDescription: taskDescription)
+                        
                     }
-                    .buttonStyle(BorderlessButtonStyle())
-                    .listStyle(.insetGrouped)
-                    .scrollContentBackground(.hidden)
-                    .background(Color.black)
+                    .listRowInsets(.init(top: 0,
+                                         leading: 0,
+                                         bottom: 0,
+                                         trailing: 0))
+                    
                 }
             }
+            .buttonStyle(BorderlessButtonStyle())
+            .listStyle(.insetGrouped)
+            .scrollContentBackground(.hidden)
+            .background(Color.black)
+            .navigationTitle("Tasks")
+            .toolbarBackground(.visible)
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .searchable(text: $searchText)
         }
+        
     }
 }
 
